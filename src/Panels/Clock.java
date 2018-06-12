dc89766494b3478a4e7983c197788f716c4682a0
emilel <limeliasson@gmail.com>
2018-06-12 20:50:35 +0200

    added (static) time

--- src/Panels/Clock.java
+++ src/Panels/Clock.java
@@ -4,9 +4,15 @@ import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.text.Font;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Clock {
     public static Node get() {
-        Label clock = new Label("13:37");
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        Date date = new Date();
+        Label clock = new Label(dateFormat.format(date));
         clock.setFont(Font.font(48));
         return clock;
     }
